dae019a8f873aea280c5d8742168fca56df2f6aa
zukizukizuki <zukizuki.git@gmail.com>
2023-08-12 17:42:58 +0900

    CORS_ORIGINの指定

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,5 @@
 // API の URL を 'http://203.0.113.10' のように指定してください。
-const API_URL_PREFIX = ''
+const API_URL_PREFIX = 'http://3.115.25.32'
 
 const input1Element = document.getElementById('input1')
 const input2Element = document.getElementById('input2')
